0e616a6be278a87bdf36814fdac186e336268d60
novexislabs07 <novexislabs07@example.com>
2026-03-09 15:30:39 +0530

    Fix base path and update founder roles in index.html

--- novexis-labs/vite.config.js
+++ novexis-labs/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/<REPO_NAME>/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Novaxislabs/' : '/',
   plugins: [react()],
 })
