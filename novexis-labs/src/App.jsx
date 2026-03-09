56ec69680a8f01af26fd9c88e3bd3831c3925d72
novexislabs07 <novexislabs07@example.com>
2026-03-09 16:21:54 +0530

    Switched to HashRouter to fix 404 on refresh

--- novexis-labs/src/App.jsx
+++ novexis-labs/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -20,7 +20,7 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <Router basename="/Novaxislabs">
+    <Router>
       <ScrollToTop />
       <Navbar />
       <Routes>
